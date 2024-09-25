ed3d39d6c1e44b4dd3c1fc2bc2fa5928fc43eaf3
Bhupendra Raut <braut@anl.gov>
2024-09-25 10:02:00 -0500

    plotting added, comments updated

--- bash/process_smartflux_data.sh
+++ bash/process_smartflux_data.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# The script automates the daily processing of SmartFlux data from a waggle-dev-node-w096
+# The script automates the daily processing of SmartFlux data from a waggle-dev-node
 # to the CELS GCE server in directory `/nfs/gce/projects/crocus/CMS-FLX-001-UIC`. 
-# Each day, the log file is reset, rsync the data and processes to the previous day, 
-# checking whether NetCDF files for raw and results data already exist. If not, it calls 
-# the processing scripts (read_raw_toNC.py and read_results_toNC.py) and logs errors if processing fails.
-# Output NetCDF files are organized in subdirectories named by year and month (%Y%m) for ingest. 
+# The log file is reset everytime the script runs, we rsync the data and process till the day before, 
+# Also checks if NetCDF files for raw and results data already exist, if not, it will try to process using
+# `read_raw_toNC.py` and `read_results_toNC.py`. Output NetCDF files are saved in CL61 style for ingest. 
 
 ROOT_DIR="/Users/bhupendra/projects/crocus/data/flux_data/data"
 LOG_FILE="$ROOT_DIR/daily_flux_data_processing.log"
@@ -13,15 +12,17 @@ NC_DIR="$ROOT_DIR/netcdf"
 SYNC_SOURCE="waggle@waggle-dev-node-w096:/home/waggle/bhupendra/flux_data/data/"
 RAW_PROCESS_SCRIPT="/Users/bhupendra/projects/crocus/code/Python/licor/read_raw_toNC.py"
 RESULTS_PROCESS_SCRIPT="/Users/bhupendra/projects/crocus/code/Python/licor/read_results_toNC.py"
+PLOT_SCRIPT="/Users/bhupendra/projects/crocus/code/Python/licor/make_quicklooks.py"
 GCE_DESTINATION="braut@homes.cels.anl.gov:/nfs/gce/projects/crocus/CMS-FLX-001-UIC/"
 FILE_PREFIX="crocus-uic-smartflux"
+
 
 > $LOG_FILE
 
 
 # Start SSH agent and add the key
-eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_rsa # ~/.ssh/id_rsa_no_passphrase if did not work
+#eval $(ssh-agent -s)
+#ssh-add ~/.ssh/id_rsa # ~/.ssh/id_rsa_no_passphrase if did not work
 
 
 TODAY=$(date +"%Y-%m-%d")
@@ -67,13 +68,22 @@ while [[ "$CURRENT_DATE" < "$END_DATE" || "$CURRENT_DATE" == "$END_DATE" ]]; do
         echo "[$(date +"%Y-%m-%d %H:%M:%S")] Results data already processed for $CURRENT_DATE, skipping..." | tee -a $LOG_FILE
     fi
 
+    # Generate Quick looks
+    if [ -f "$RESULTS_NC_FILE" ]; then
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] Generating quick-lookd for $RESULTS_NC_FILE..." | tee -a $LOG_FILE
+        python $PLOT_SCRIPT --nc_file "$RESULTS_NC_FILE" --output_dir "$NC_DIR/resnc/$YEAR_MONTH" --prefix "quicklook" | tee -a $LOG_FILE 2>&1
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] Quick-look plots generated and saved." | tee -a $LOG_FILE
+    else
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] ERROR: Results NetCDF file not found for $CURRENT_DATE!" | tee -a $LOG_FILE
+    fi
+
     CURRENT_DATE=$(date -j -v+1d -f "%Y-%m-%d" "$CURRENT_DATE" +"%Y-%m-%d")
 done
 
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Syncing processed NetCDF files to GCE..." | tee -a $LOG_FILE
 rsync -azP -e "ssh -o StrictHostKeyChecking=no" $NC_DIR/* $GCE_DESTINATION >> $LOG_FILE 2>&1
 
-eval $(ssh-agent -k)
+# eval $(ssh-agent -k)
 
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Processing complete!" | tee -a $LOG_FILE
 
